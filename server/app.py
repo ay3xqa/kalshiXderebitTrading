21304c9e6aec180dc672f91e87d204a986fa8fb6
ay3xqa <ay3xqa@virginia.edu>
2024-12-30 15:02:19 -0500

    feat: historical data setup

--- server/app.py
+++ server/app.py
@@ -71,4 +71,4 @@ def get_all_kalshi_markets_json():
 
 if __name__ == '__main__':
     # For local development
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5001)), debug=True)
